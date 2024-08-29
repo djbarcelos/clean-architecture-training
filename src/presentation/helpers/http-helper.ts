a3657f6c58d70a898a42b079189977eb6b823591
djbarcelos <djbarcelos@gmail.com>
2024-08-29 15:00:42 -0300

    refactor: change import and change method scope ok

--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -1,18 +1,17 @@
-import { type AccountModel } from '../controllers/signup/signup-protocols'
-import { ServerError } from '../error/server-error'
-import { type HttpResponse } from '../protocols/http'
+import { type HttpResponse } from '../protocols'
+import { ServerError } from '../error'
 
 export const badRequest = (error: Error): HttpResponse => ({
   statusCode: 400,
   body: error
 })
 
-export const ok = (account: AccountModel): HttpResponse => ({
-  statusCode: 200,
-  body: account
-})
-
 export const serverError = (): HttpResponse => ({
   statusCode: 500,
   body: new ServerError()
+})
+
+export const ok = (data: any): HttpResponse => ({
+  statusCode: 200,
+  body: data
 })
