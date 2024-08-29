78cac736868af4fde4013d4ab97f3dd69da40a1e
djbarcelos <djbarcelos@gmail.com>
2024-08-28 23:04:40 -0300

    refactor: add custom error for missing param

--- jest.config.ts
+++ jest.config.ts
@@ -3,7 +3,13 @@ import type { Config } from 'jest'
 const config: Config = {
   roots: ['<rootDir>/src'],
   collectCoverage: true,
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts', // Inclui todos os arquivos .ts na pasta src
+    '!<rootDir>/src/**/*.spec.ts', // Exclui arquivos de teste
+    '!<rootDir>/src/**/*.test.ts', // Exclui arquivos de teste
+    '!<rootDir>/src/**/*.d.ts', // Exclui arquivos de declaração TypeScript
+    '!<rootDir>/src/**/*.mock.ts' // Exclui arquivos de mock, se existirem
+  ],
   coverageDirectory: 'coverage',
   coverageProvider: 'v8',
   transform: {
