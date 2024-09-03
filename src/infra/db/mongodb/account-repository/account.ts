0f598836fc70d9ac886a837cf7d670cf6d880c23
djbarcelos <djbarcelos@gmail.com>
2024-09-03 12:10:45 -0300

    feat: ensure: AccountMongoRepository returns an account on success

---
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -0,0 +1,17 @@
+import { type WithId } from 'mongodb'
+import { type AddAccountRepository } from '../../../../data/protocols/add-account-repository'
+import { type AccountModel } from '../../../../domain/models/account'
+import { type AddAccountModel } from '../../../../domain/usecases/add-account'
+import { MongoHelper } from '../helpers/mongo-helper'
+
+export class AccountMongoRepository implements AddAccountRepository {
+  async add (accountData: AddAccountModel): Promise<AccountModel> {
+    const accountCollection = MongoHelper.getCollection('accounts')
+    const result = await accountCollection.insertOne(accountData)
+    const accountDocument = await accountCollection.findOne({ _id: result.insertedId })
+
+    const { _id, ...accountWithoutId } = accountDocument as unknown as WithId<AccountModel>
+
+    return Object.assign({}, accountWithoutId, { id: result.insertedId.toString() })
+  }
+}
--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -1,17 +1,22 @@
-import { MongoClient } from 'mongodb'
+import { type Collection, Db, MongoClient } from 'mongodb'
+
+const MONGO_URL = 'mongodb://localhost:27017/'
+const DB_NAME = 'defaultdb'
 
 export const MongoHelper = {
   client: null as unknown as MongoClient,
+  db: Db,
 
   async connect (uri: string): Promise<void> {
-    this.client = await MongoClient.connect(global.__MONGO_URI__, {
-      // @ts-expect-error
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    })
+    this.client = await MongoClient.connect(MONGO_URL, {})
+    this.db = this.client.db(DB_NAME)
   },
 
   async disconnect (): Promise<void> {
     await this.client.close()
+  },
+
+  getCollection (name: string): Collection {
+    return this.db.collection(name)
   }
 }
